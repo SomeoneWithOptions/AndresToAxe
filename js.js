4a1c94e51a1625c794b4ccb249dae759acad3cdf
SomeoneWithOptions <andrescastesol@gmail.com>
2022-10-17 19:11:23 -0500

    Update js.js

--- js.js
+++ js.js
@@ -9,17 +9,34 @@ const hours = minutes * 60 ;
 const days =  hours * 24 ;
 const years = days * 365 ;
 
-
-console.log(fiveYears)
-console.log(currentDate)
-
 document.getElementById('years').innerText = Math.floor(gap/years); 
 document.getElementById('days').innerText = Math.floor((gap % years) / days); 
 document.getElementById('hours').innerText = Math.floor((gap % days)/ hours); 
 document.getElementById('minutes').innerText = Math.floor((gap % hours) / minutes); 
 document.getElementById('seconds').innerText = Math.floor((gap % minutes) / seconds); 
+}
 
+function changeColors(){
+  const colors = {
+    1:['#f6178e','#ffffff'],
+    2:['#3dfb8b','#141414'],
+    3:['#1859C2','#ffffff'],
+    4:['#BDE0FE','#141414'],
+    5:['#FFEF88','#141414'],
+    6:['#000000','#ffffff'],
+    7:['#ffd6ff','#141414']
+  }
+  let picked = Math.floor(Math.random()*7)+1
 
+  let background = colors[picked][0]
+  let fontColor = colors[picked][1]
+
+  console.log (background,fontColor)
+
+  let body = document.getElementsByTagName('body')[0]
+  body.style.backgroundColor = background
+  body.style.color = fontColor
 }
 
 setInterval(() => tooLong(),1000);
+setInterval(() => randomStyles(),(1000*60*60*24))
